53bebf9e8c42a53e6f76ab6d8c9b8cf2828e2baf
Geovanna Rebecca <geovannatapias0509@gmail.com>
2024-10-01 18:46:13 +0100

    MySQL adaptation and first migration

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+    options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"),
+    new MySqlServerVersion(new Version(8, 0, 38)), // Ajuste para sua versão do MySQL
+    mysqlOptions => mysqlOptions.MigrationsAssembly("SalesWebMvc")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
